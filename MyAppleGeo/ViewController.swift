a003f5660629e3c79f2c8fdf9e9c7a44cf1918a1
Jengking <mfadzleyhud@yahoo.com>
2022-05-31 11:00:05 +0800

    init commit

---
+++ MyAppleGeo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAppleGeo
+//
+//  Created by Fadzley Hud on 31/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
